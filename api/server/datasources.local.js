363dcbd2438a706016df6dc075f385ee4ffd053d
Antoine de Chassey <antoinedechassey@gmail.com>
2017-08-18 11:22:46 +0200

    [Feature] - Add MongoDB connector
    Signed-off-by: Antoine de Chassey <antoinedechassey@gmail.com>

---
+++ api/server/datasources.local.js
@@ -0,0 +1,13 @@
+'use strict';
+const mongodbURI = process.env.MONGODB_URI;
+
+if(mongodbURI) {
+  console.log('Data sources: Using MongoDB config', mongodbURI)
+  module.exports = {
+    "db": {
+      "url": mongodbURI,
+      "name": "mongodb",
+      "connector": "mongodb"
+    }
+  }
+}
