472111ff365be78719544f91ea18a3b6d59ac524
Louis Moreau <louis.moreau@sigfox.com>
2017-10-30 17:37:52 +0100

    [Merge] - merged local and server

---
+++ api/tests/organization.spec.ts
@@ -0,0 +1,11 @@
+var should    = require('chai').should();
+var supertest = require('supertest');
+var api       = supertest('http://localhost:3000/api');
+
+describe('organization unit tests:', () => {
+    it('Should create a organization instance', (done: Function) => {
+        api.post('/organizations').send({
+            name: 'test'
+        }).expect(200, done);
+    });
+});
