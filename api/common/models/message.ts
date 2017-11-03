46adc951e39de76ee2764d666fb7aded14d9f412
Antoine de Chassey <antoinedechassey@gmail.com>
2017-11-03 18:07:11 +0100

    [Fix] - Geoloc array is now created if absent, all Sigfox Backend callbacks working
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- api/common/models/message.ts
+++ api/common/models/message.ts
@@ -197,16 +197,14 @@ class Message {
 
           // Now checking where geoloc sigfox is in the location array so it can be updated
           let entryGeoloc_sigfox = false;
-          if(deviceInstance.location){
-            deviceInstance.location.forEach((geoloc: Geoloc, index) => {
-              if(geoloc.type === "geoloc_sigfox"){
-                deviceInstance.location[index] = geoloc_sigfox; // Replace geoloc_sigfox with new one
-                entryGeoloc_sigfox = true;
-              }
-            });
-          } else {
+          if(!deviceInstance.location)
             deviceInstance.location = [];
-          }
+          deviceInstance.location.forEach((geoloc: Geoloc, index) => {
+            if(geoloc.type === "geoloc_sigfox"){
+              deviceInstance.location[index] = geoloc_sigfox; // Replace geoloc_sigfox with new one
+              entryGeoloc_sigfox = true;
+            }
+          });
           if(!entryGeoloc_sigfox)
             deviceInstance.location.push(geoloc_sigfox);
 
@@ -239,6 +237,8 @@ class Message {
         } else {
           if(message){
             console.log("Found the corresponding message and storing geoloc_sigfox in it");
+            if(!message.geoloc)
+              message.geoloc = [];
             message.geoloc.push(geoloc_sigfox);
             ctx.args.data = message;
           } else {
