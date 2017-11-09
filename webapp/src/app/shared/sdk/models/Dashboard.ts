13fe45019a24440f8680187f354ba123d16e0eff
Antoine de Chassey <antoinedechassey@gmail.com>
2017-11-09 17:08:22 +0100

    [Fix] - Generated new loopback SDK client (Dashboard model)
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

---
+++ webapp/src/app/shared/sdk/models/Dashboard.ts
@@ -0,0 +1,75 @@
+/* tslint:disable */
+
+declare var Object: any;
+export interface DashboardInterface {
+  "name": string;
+  "description"?: string;
+  "id"?: number;
+  "createdAt"?: Date;
+  "updatedAt"?: Date;
+}
+
+export class Dashboard implements DashboardInterface {
+  "name": string = '';
+  "description": string = '';
+  "id": number = 0;
+  "createdAt": Date = new Date(0);
+  "updatedAt": Date = new Date(0);
+  constructor(data?: DashboardInterface) {
+    Object.assign(this, data);
+  }
+  /**
+   * The name of the model represented by this $resource,
+   * i.e. `Dashboard`.
+   */
+  public static getModelName() {
+    return "Dashboard";
+  }
+  /**
+  * @method factory
+  * @author Jonathan Casarrubias
+  * @license MIT
+  * This method creates an instance of Dashboard for dynamic purposes.
+  **/
+  public static factory(data: DashboardInterface): Dashboard{
+    return new Dashboard(data);
+  }
+  /**
+  * @method getModelDefinition
+  * @author Julien Ledun
+  * @license MIT
+  * This method returns an object that represents some of the model
+  * definitions.
+  **/
+  public static getModelDefinition() {
+    return {
+      name: 'Dashboard',
+      plural: 'Dashboards',
+      path: 'Dashboards',
+      properties: {
+        "name": {
+          name: 'name',
+          type: 'string'
+        },
+        "description": {
+          name: 'description',
+          type: 'string'
+        },
+        "id": {
+          name: 'id',
+          type: 'number'
+        },
+        "createdAt": {
+          name: 'createdAt',
+          type: 'Date'
+        },
+        "updatedAt": {
+          name: 'updatedAt',
+          type: 'Date'
+        },
+      },
+      relations: {
+      }
+    }
+  }
+}
