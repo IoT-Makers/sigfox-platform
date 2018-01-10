8404b733cfe21edc74d4007cf692678ebb516e37
AntoineDeChassey <antoinedechassey@gmail.com>
2018-01-10 18:01:38 +0100

    [Fix] - Filter RegExp
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- webapp/src/app/pages/messages/datafilterpipe.ts
+++ webapp/src/app/pages/messages/datafilterpipe.ts
@@ -7,27 +7,25 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class DataFilterPipe implements PipeTransform {
 
   transform(array: any[], query: string): any {
-    if (query) {
+    // Variables
+    let regExp = new RegExp('.[*].', 'ig');
+    if (regExp.test(query)) {
+      const startQuery = query.substring(0, query.indexOf('*'));
+      const endQuery = query.substring(query.indexOf('*') + 1, query.length);
+      query = query.replace('*', '');
+      regExp = new RegExp(startQuery + '[A-Za-z0-9]{1}' + endQuery, 'ig');
+      return _.filter(array, row => {
+        //console.log(row.deviceId.match(regExp));
+        // Device ID
+        const hasDeviceId = row.deviceId && regExp.test(row.deviceId);
+        return hasDeviceId;
+      });
+    } else if (query) {
       return _.filter(array, row => {
         // Data
         const hasData = row.data && row.data.indexOf(query) > -1;
         // Device ID
         const hasDeviceId = row.deviceId && row.deviceId.indexOf(query.toUpperCase().replace(/\s/g, '')) > -1;
-/*
-        // Variables
-        let regExp = new RegExp('.[*].', 'ig');
-        console.log(query.match(regExp));
-        if (query.match(regExp) !== null) {
-          const startQuery = query.substring(0, query.indexOf('*'));
-          const endQuery = query.substring(query.indexOf('*') + 1, query.length);
-          query = query.replace('*', '');
-
-          regExp = new RegExp(startQuery + '[A-Za-z0-9]{1}' + endQuery, 'ig');
-          //console.log(row.deviceId.match(regExp));
-          hasDeviceId = row.deviceId.match(regExp);
-        }
-*/
-
         return hasDeviceId || hasData;
       });
     }
