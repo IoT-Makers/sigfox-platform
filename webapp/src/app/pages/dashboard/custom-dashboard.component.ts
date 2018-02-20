f4a38e7f3ca4450ce7263210be4c9c275bf1e90c
AntoineDeChassey <antoinedechassey@gmail.com>
2018-02-20 12:19:40 +0100

    [Fix] - Disabled Real Time
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- webapp/src/app/pages/dashboard/custom-dashboard.component.ts
+++ webapp/src/app/pages/dashboard/custom-dashboard.component.ts
@@ -596,9 +596,9 @@ export class CustomDashboardComponent implements OnInit, OnDestroy {
         this.widgets.forEach(widget => {
           // Real-Time
           // Devices
-          this.deviceRef = this.rt.FireLoop.ref<Device>(Device);
-          this.deviceRef.on('change', widget.filter).subscribe((devices: any[]) => {
-            /*this.userApi.getDevices(this.user.id, widget.filter).subscribe((devices: any[]) => {*/
+          /*this.deviceRef = this.rt.FireLoop.ref<Device>(Device);
+          this.deviceRef.on('change', widget.filter).subscribe((devices: any[]) => {*/
+          this.userApi.getDevices(this.user.id, widget.filter).subscribe((devices: any[]) => {
             devices.forEach(device => {
               device.visibility = false;
               device.directionsDisplayStore = [];
@@ -733,97 +733,5 @@ export class CustomDashboardComponent implements OnInit, OnDestroy {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-
-  /********************************************
-   *
-   *                TRACKING
-   *
-   *******************************************/
-  /*showTracking(device: any, travelMode: string): void {
-    if (device.visibility) {
-      this.buildDirections(device.Messages, device.directionsDisplayStore, device.color, travelMode);
-    }
-  }
-
-  buildDirections(geolocMessages: Message[], directionsDisplayStore: any, routesColor: string, travelMode: string) {
-    console.log('--------------------------------');
-    console.log(directionsDisplayStore);
-    for (const i in directionsDisplayStore) {
-      directionsDisplayStore[i].setMap(null);
-    }
-    directionsDisplayStore = [];
-    console.log('--------------------------------');
-
-    let messages: Message[] = [];
-    const startCoord = geolocMessages[0].geoloc[0];
-    const endCoord = geolocMessages[geolocMessages.length - 1].geoloc[0];
-
-    const routes = Math.floor(geolocMessages.length / 23) !== 0 ? Math.floor(geolocMessages.length / 23) : 1;
-    console.log('Number of routes (23 geolocs per routes)', routes);
-    for (let i = 0; i < routes; i++) {
-      let waypoints = [];
-      messages = geolocMessages.slice(i * 23, (i + 1) * 23);
-      /!*console.log("i*23", i*23);
-      console.log("(i+1)*23-1", (i+1)*23);
-
-      console.log("messagesCount", messages.length);
-      console.log(messages);*!/
-
-      messages.forEach((message: Message) => {
-        message.geoloc.forEach((geoloc: Geoloc) => {
-          const latLng = new google.maps.LatLng(geoloc.lat, geoloc.lng);
-          waypoints.push({location: latLng, stopover: true});
-        });
-      });
-
-      //console.log(waypoints);
-
-      if (waypoints.length <= 23) {
-
-        this._googleMapsAPIWrapper.getNativeMap().then((map) => {
-          const directionsService = new google.maps.DirectionsService;
-          const directionsDisplay = new google.maps.DirectionsRenderer;
-          directionsDisplay.setMap(map);
-          directionsDisplayStore.push(directionsDisplay);
-          directionsDisplay.setOptions({
-            polylineOptions: {
-              strokeWeight: 4,
-              strokeOpacity: 0.5,
-              strokeColor: routesColor ? routesColor : this.getRandomRolor()
-            },
-            markerOptions: {
-              visible: false
-            }
-          });
-
-          // If travel mode is TRANSIT, then only use the starting and ending coordinates
-          if (travelMode === 'TRANSIT') {
-            waypoints = [];
-          }
-
-          directionsService.route({
-            origin: {lat: startCoord.lat, lng: startCoord.lng},
-            destination: {lat: endCoord.lat, lng: endCoord.lng},
-            waypoints: waypoints,
-            optimizeWaypoints: false,
-            avoidHighways: false,
-            travelMode: travelMode
-          }, function (response, status) {
-            if (status === 'OK') {
-              //console.log(response);
-              directionsDisplay.setDirections(response);
-            } else {
-              console.log(status);
-              //window.alert('Directions request failed due to ' + status);
-            }
-          });
-        }, err => {
-          console.log('error', err);
-        });
-
-      }
-
-    }
-  }*/
 }
 
