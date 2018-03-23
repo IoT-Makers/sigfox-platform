4a986b50c3bb36695698a76fcdf83c48a739bac8
AntoineDeChassey <antoinedechassey@gmail.com>
2018-03-23 16:22:01 +0100

    [Fix] Typings
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- webapp/src/typings.d.ts
+++ webapp/src/typings.d.ts
@@ -1,11 +1,12 @@
 // Typings reference file, see links for more information
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
-import * as L from 'leaflet';
 
 declare var System: any;
+
+/*import * as L from 'leaflet';
 declare module 'leaflet' {
   namespace control {
     function fullscreen(v: any);
   }
-}
+}*/
