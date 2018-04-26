cfd46c369993364ca860ca7ae27576894e2769b4
AntoineDeChassey <antoinedechassey@gmail.com>
2018-04-26 12:08:17 +0200

    [Update] Sensit parser clean with alerts counter
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- doc/parsers/sensit.js
+++ doc/parsers/sensit.js
@@ -17,6 +17,7 @@ while (byte.length < 8)
     byte = '0' + byte;
 battery = byte.slice(0, 1);
 type = parseInt(byte.slice(1, 3), 2);
+
 switch (type) {
     case 0:
         type = 'Classic';
@@ -57,18 +58,66 @@ switch (mode) {
         break;
     case 1:
         mode = 'Temperature & Humidity';
+        // Byte #2
+        var byte = parseInt(payload.slice(2, 4), 16).toString(2);
+        while (byte.length < 8)
+            byte = '0' + byte;
+        temperature = byte.slice(0, 4);
+        // Byte #3
+        var byte = parseInt(payload.slice(4, 6), 16).toString(2);
+        while (byte.length < 8)
+            byte = '0' + byte;
+        temperature += byte.slice(2, 8);
+        temperature = ((parseInt(temperature, 2) - 200) / 8).toFixed(2);
+
+        if (type === 'Button')
+        // Byte #4
+            humidity = parseInt(payload.slice(6, 8), 16) * 0.5;
+        else {
+            // Byte #4
+            var byte = parseInt(payload.slice(6, 8), 16).toString(2);
+            while (byte.length < 8)
+                byte = '0' + byte;
+            firmwareVersion = parseInt(byte.slice(0, 3), 2) + '.' + parseInt(byte.slice(4, 8), 2);
+        }
         break;
     case 2:
         mode = 'Light';
+        // Byte #3
+        var byte = parseInt(payload.slice(4, 6), 16).toString(2);
+        while (byte.length < 8)
+            byte = '0' + byte;
+        var multiplier = parseInt(byte.slice(0, 2), 2);
+        switch (multiplier) {
+            case 0:
+                multiplier = 1;
+                break;
+            case 1:
+                multiplier = 8;
+                break;
+            case 2:
+                multiplier = 64;
+                break;
+            case 3:
+                multiplier = 2014;
+                break;
+            default:
+                multiplier = 'Unknown multiplier {' + multiplier + '}';
+        }
+        light = parseInt(byte.slice(2, 8), 2);
+        light = multiplier * light * 0.01;
         break;
     case 3:
         mode = 'Door';
+        alert = parseInt(payload.slice(6, 8), 16);
         break;
     case 4:
         mode = 'Move';
+        alert = parseInt(payload.slice(6, 8), 16);
         break;
     case 5:
         mode = 'Reed switch';
+        alert = parseInt(payload.slice(6, 8), 16);
         break;
     default:
         mode = 'Unknown mode {' + mode + '}';
@@ -80,65 +129,6 @@ while (byte.length < 8)
     byte = '0' + byte;
 battery += byte.slice(4, 8);
 battery = (parseInt(battery, 2) * 0.05 + 2.7).toFixed(2);
-
-// Temperature & Humidity
-if (mode === 'Temperature & Humidity') {
-    // Byte #2
-    var byte = parseInt(payload.slice(2, 4), 16).toString(2);
-    while (byte.length < 8)
-        byte = '0' + byte;
-    temperature = byte.slice(0, 4);
-    // Byte #3
-    var byte = parseInt(payload.slice(4, 6), 16).toString(2);
-    while (byte.length < 8)
-        byte = '0' + byte;
-    temperature += byte.slice(2, 8);
-    temperature = ((parseInt(temperature, 2) - 200) / 8).toFixed(2);
-
-    if (type !== 'Button')
-    // Byte #4
-        humidity = parseInt(payload.slice(6, 8), 16) * 0.5;
-    else {
-        // Byte #4
-        var byte = parseInt(payload.slice(6, 8), 16).toString(2);
-        while (byte.length < 8)
-            byte = '0' + byte;
-        firmwareVersion = parseInt(byte.slice(0, 3), 2) + '.' + parseInt(byte.slice(4, 8), 2);
-    }
-}
-
-// Light
-if (mode === 'Light') {
-    // Byte #3
-    var byte = parseInt(payload.slice(4, 6), 16).toString(2);
-    while (byte.length < 8)
-        byte = '0' + byte;
-    var multiplier = parseInt(byte.slice(0, 2), 2);
-    switch (multiplier) {
-        case 0:
-            multiplier = 1;
-            break;
-        case 1:
-            multiplier = 8;
-            break;
-        case 2:
-            multiplier = 64;
-            break;
-        case 3:
-            multiplier = 2014;
-            break;
-        default:
-            multiplier = 'Unknown multiplier {' + multiplier + '}';
-    }
-    light = parseInt(byte.slice(2, 8), 2);
-    light = multiplier * light * 0.01;
-}
-
-// Alert (Door - Move - Reed switch)
-if (mode === 'Door' || mode === 'Move' || mode === 'Reed switch')
-// Byte #4
-    alert = parseInt(payload.slice(6, 8), 16) === 0 ? false : true;
-
 
 // Store objects in parsedData array
 obj = {};
