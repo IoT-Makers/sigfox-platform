acaac9fa5def47560debe7128db661048eba8782
AntoineDeChassey <antoinedechassey@gmail.com>
2018-04-27 13:08:22 +0200

    [Feature] aes-256-cbc
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- api/common/models/utils.ts
+++ api/common/models/utils.ts
@@ -1,7 +1,7 @@
 // Nodejs encryption with CTR
-const crypto = require('crypto'),
-  algorithm = 'aes-256-ctr',
-  password = process.env.SECRET || 'sgx/Platform2018.!';
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const password = process.env.SECRET || 'sgx/Platform2018.!';
 
 export function encrypt(text: string) {
   const cipher = crypto.createCipher(algorithm, password);
