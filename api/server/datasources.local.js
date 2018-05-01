8a80323758d6e3a54596e3f26bd0dbc1c5cfac74
AntoineDeChassey <antoinedechassey@gmail.com>
2018-05-01 15:46:13 +0200

    [Update] Dokku mongo:link URL works w/ MONGO_URL scheme
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- api/server/datasources.local.js
+++ api/server/datasources.local.js
@@ -1,13 +1,14 @@
 'use strict';
-var mongodbURI = process.env.MONGODB_URI;
 
-if(mongodbURI) {
+var mongodbURI = process.env.MONGO_URL || process.env.MONGODB_URI;
+
+if (mongodbURI) {
   console.log('Data sources: Using MongoDB config', mongodbURI);
   module.exports = {
-    "mongodb": {
-      "url": mongodbURI,
-      "name": "mongodb",
-      "connector": "mongodb"
+    'mongodb': {
+      'url': mongodbURI,
+      'name': 'mongodb',
+      'connector': 'mongodb'
     }
-  }
+  };
 }
