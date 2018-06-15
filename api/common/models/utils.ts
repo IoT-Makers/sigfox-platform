ce2a63bf7bc4ef414a63d3e4a0dbfbc654f6ae94
AntoineDeChassey <antoinedechassey@gmail.com>
2018-06-15 16:14:26 +0200

    [Add] Payload encryption init
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- api/common/models/utils.ts
+++ api/common/models/utils.ts
@@ -5,10 +5,15 @@ const password = process.env.SECRET || 'sgx/Platform2018.!';
 
 export function encrypt(text: string) {
   if (text) {
-    const cipher = crypto.createCipher(algorithm, password);
-    let crypted = cipher.update(text, 'utf8', 'hex');
-    crypted += cipher.final('hex');
-    return crypted;
+    try {
+      const cipher = crypto.createCipher(algorithm, password);
+      let crypted = cipher.update(text, 'utf8', 'hex');
+      crypted += cipher.final('hex');
+      return crypted;
+    } catch (e) {
+      console.error(e);
+      return '';
+    }
   } else {
     return '';
   }
@@ -30,7 +35,59 @@ export function decrypt(text: string) {
   }
 }
 
+export function encryptPayload(payload: string, pek: string, ctr: string) {
+  if (payload && pek && ctr) {
+    try {
+      const cipher = crypto.createCipheriv('aes-128-ecb', Buffer.from(pek, 'hex'), '');
+      //cipher.setAutoPadding(false);
+      let sessionKey: any = cipher.update(Buffer.from(ctr, 'hex')).toString('hex');
+      sessionKey += cipher.final().toString('hex');
+      sessionKey = sessionKey.substr(0, payload.length);
+      return xor(Buffer.from(payload, 'hex'), Buffer.from(sessionKey, 'hex')).toString('hex');
+    } catch (e) {
+      console.error(e);
+      return '';
+    }
+  } else {
+    return '';
+  }
+}
+
+export function decryptPayload(payload: string, pek: string, ctr: string) {
+  if (payload && pek && ctr) {
+    try {
+      const decipher = crypto.createDecipheriv('aes-128-ecb', Buffer.from(pek, 'hex'), '');
+      //decipher.setAutoPadding(false);
+      let sessionKey = decipher.update(Buffer.from(ctr, 'hex')).toString('hex');
+      sessionKey += decipher.final().toString('hex');
+      sessionKey = sessionKey.substr(0, payload.length);
+      return xor(Buffer.from(payload, 'hex'), Buffer.from(sessionKey, 'hex')).toString('hex');
+    } catch (e) {
+      console.error(e);
+      return '';
+    }
+  } else {
+    return '';
+  }
+}
+
 export function generateVerificationToken(): string {
   return crypto.randomBytes(64).toString('hex');
+}
+
+export function xor(a: Buffer, b: Buffer) {
+  if (!Buffer.isBuffer(a)) a = new Buffer(a);
+  if (!Buffer.isBuffer(b)) b = new Buffer(b);
+  const res = [];
+  if (a.length > b.length) {
+    for (let i = 0; i < b.length; i++) {
+      res.push(a[i] ^ b[i]);
+    }
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      res.push(a[i] ^ b[i]);
+    }
+  }
+  return new Buffer(res);
 }
 
--- api/server/boot/01-load-app-settings.ts
+++ api/server/boot/01-load-app-settings.ts
@@ -1,4 +1,39 @@
+import {decryptPayload, encryptPayload} from '../../common/models/utils';
+
 module.exports = (app: any) => {
+
+  let deviceId = parseInt('02310c92', 16).toString(2);
+  while (deviceId.length < 32) {
+    deviceId = '0' + deviceId;
+  }
+  /*deviceId = parseInt(deviceId, 2).toString(16);
+  console.log(deviceId);*/
+
+  const uplinkOrDownlink = '0';
+
+  let seqNumber = parseInt('3844', 10).toString(2);
+  while (seqNumber.length < 12) {
+    seqNumber = '0' + seqNumber;
+  }
+  /*seqNumber = parseInt(seqNumber, 2).toString(16);
+  console.log(seqNumber);*/
+
+  let padding = '';
+  while (padding.length < 83) {
+    padding = '0' + padding;
+  }
+
+  let ctr = deviceId + uplinkOrDownlink + seqNumber + padding;
+  ctr = parseInt(ctr, 2).toString(16);
+  while (ctr.length < 32) {
+    ctr = '0' + ctr;
+  }
+  const pek = 'bc78a84252adff85967a01ddd5e38370';
+  const payload = '5f03330e94ba63d138';
+  console.log(encryptPayload(payload, pek, ctr));
+
+  const encryptedPayload = 'ad8ee74338d77510';
+  console.log(decryptPayload(encryptedPayload, pek, ctr));
 
   const AppSetting = app.models.AppSetting;
   let countAppSettings = 0;
