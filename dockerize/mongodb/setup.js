7d36754ab42004f93ba1656e1835d38a21ccd77e
Siyu ZHANG <astro6974@gmail.com>
2018-09-07 15:38:34 +0200

    Dockerize backend

--- dockerize/mongodb/setup.js
+++ dockerize/mongodb/setup.js
@@ -1,3 +1,7 @@
 // these ops happen in MONGO_INITDB_DATABASE
 db.createCollection('user',{})
-db.createUser({ user: "root", pwd: "example", roles: [ { role: "root", db: "admin" }]})
+db.createUser({ 
+    user: "usr", pwd: "pwd", 
+    roles: [{ role: "root", db: "admin" }]
+    }
+)
