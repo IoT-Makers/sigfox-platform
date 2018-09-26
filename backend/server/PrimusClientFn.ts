34ff03518fd73300601491c61ba723ae55428708
Siyu ZHANG <astro6974@gmail.com>
2018-09-26 12:33:09 +0200

    Clean

--- backend/server/PrimusClientFn.ts
+++ backend/server/PrimusClientFn.ts
@@ -1,11 +1,11 @@
-const Primus = require("primus");
+const Primus = require('primus');
 
 
 export class PrimusClientFn {
   public static newClient() {
     let Socket = Primus.createSocket({ transformer: 'engine.io' });
-    const primusURL = process.env.PRIMUS_URL || "http://localhost:2333";
-    let primusClient = new Socket(primusURL + "?access_token=" + process.env.SERVER_ACCESS_TOKEN);
+    const primusURL = process.env.PRIMUS_URL || 'http://localhost:2333';
+    let primusClient = new Socket(primusURL + '?access_token=' + process.env.SERVER_ACCESS_TOKEN);
 
     primusClient.on('close', () => {
       console.warn('close');
