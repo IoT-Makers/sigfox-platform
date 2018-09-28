592edb97486e93b3e0f23f82f600a9f3710e8d49
antoine <antoinedechassey@gmail.com>
2018-09-28 16:43:18 +0200

    [Update] Env variables
    Signed-off-by: antoine <antoinedechassey@gmail.com>

---
+++ frontend/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  envName: 'prod',
+  apiUrl: 'http://dev.iotagency.com:3000',
+  apiVersion: 'api',
+  PRIMUS_URL: 'http://dev.iotagency.com:2333'
+};
