41edf37b3272c47410d61f87c331544c2af58b9f
antoine <antoinedechassey@gmail.com>
2018-10-09 18:44:25 +0200

    [Update] Employee import
    Signed-off-by: antoine <antoinedechassey@gmail.com>

--- backend/server/boot/04-load-employees.ts
+++ backend/server/boot/04-load-employees.ts
@@ -6,11 +6,11 @@ module.exports = (app: any) => {
     const Employee = app.models.Employee;
     let countEmployees = 0;
 
-    Employee.destroyAll();
+    //Employee.destroyAll();
 
     csv.fromPath(__dirname + '/employees.csv')
       .on('data', (data: any) => {
-        if (data[5] === 'Staff') {
+        if (data[5] === 'Staff' || data[5] === 'STAFF') {
           countEmployees++;
           const employee = new Employee();
           employee.firstName = data[0];
@@ -19,9 +19,10 @@ module.exports = (app: any) => {
           employee.country = data[4];
           employee.email = data[6];
           //console.log(employee);
-          Employee.create(employee, (err: any, employee: any) => {
+          Employee.findOrCreate({where: {email: employee.email}}, employee, (err: any, employee: any, created: boolean) => {
             if (err) console.error(err);
-            else {}
+            else if (created) console.log('Created: ' + employee.email);
+            else console.log(employee.email + ' already exists.');
           });
         }
       })
