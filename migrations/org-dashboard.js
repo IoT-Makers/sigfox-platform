6b691b0fced405839fc4c47ca90192f020b5c5d4
Unknown <astro6974@gmail.com>
2018-10-22 10:33:52 +0200

    Log migration error

--- migrations/org-dashboard.js
+++ migrations/org-dashboard.js
@@ -4,17 +4,18 @@ const process = require('process');
 // Connection URL
 const url = process.env.MONGO_URL;
 
-// Database Name
-const dbName = 'testdb';
-
 // Create a new MongoClient
 const client = new MongoClient(url);
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
+    if (err) {
+        console.error(err);
+        return;
+    }
     console.log("Connected successfully to server");
 
-    const db = client.db(dbName);
+    const db = client.db();
 
     const OrganizationDashboard = db.collection('OrganizationDashboard');
     const Dashboard = db.collection('Dashboard');
