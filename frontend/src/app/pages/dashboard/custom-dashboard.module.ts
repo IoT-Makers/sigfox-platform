6cbf666bdd5dc324e32e9edb41a2bbfa904c1048
Antoine de Chassey <antoinedechassey@gmail.com>
2018-11-06 18:04:27 +0100

    [Fix] Date time picker

--- frontend/src/app/pages/dashboard/custom-dashboard.module.ts
+++ frontend/src/app/pages/dashboard/custom-dashboard.module.ts
@@ -8,17 +8,16 @@ import {AgmCoreModule, GoogleMapsAPIWrapper} from '@agm/core';
 import {FormsModule} from '@angular/forms';
 import {MomentModule} from 'angular2-moment';
 import {DataTableModule} from 'angular2-datatable';
-import {TooltipModule} from 'ngx-bootstrap';
+import {ModalModule, TooltipModule} from 'ngx-bootstrap';
 import {AngularMultiSelectModule} from 'angular2-multiselect-dropdown/angular2-multiselect-dropdown';
-import {AngularDateTimePickerModule} from 'vk-custom-angular2-datetimepicker';
 import {DirectionsDirective} from './directions.directive';
 import {AgmJsMarkerClustererModule} from '@agm/js-marker-clusterer';
-import {ModalModule} from 'ngx-bootstrap';
 import {ChartsModule} from 'ng2-charts/ng2-charts';
 import {NvD3Module} from 'ng2-nvd3';
 // d3 and nvd3 should be included somewhere
 import 'd3';
 import 'nvd3';
+import {OwlDateTimeModule, OwlNativeDateTimeModule} from "ng-pick-datetime";
 
 @NgModule({
   imports: [
@@ -34,7 +33,8 @@ import 'nvd3';
     ChartsModule,
     AgmJsMarkerClustererModule,
     AngularMultiSelectModule,
-    AngularDateTimePickerModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
     ModalModule.forRoot(),
     NvD3Module
   ],
