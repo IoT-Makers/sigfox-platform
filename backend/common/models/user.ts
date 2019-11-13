3d0344f19a21c910e2563dec417d67f85811ae1b
Antoine de Chassey <antoinedechassey@gmail.com>
2019-11-13 18:11:56 +0100

    update: email verification off

--- backend/common/models/user.ts
+++ backend/common/models/user.ts
@@ -35,15 +35,14 @@ const loopback = require('loopback');
 class user {
   // LoopBack model instance is injected in constructor
   constructor(public model: any) {
-    //send password reset link when password reset requested
+    // send password reset link when password reset requested
     model.on('resetPasswordRequest', function(info: any) {
       const baseUrl = process.env.BASE_URL;
       const resetUrl =
-        baseUrl + '/#/reset-password?access_token=' + info.accessToken.id;
+          baseUrl + '/#/reset-password?access_token=' + info.accessToken.id;
       // Prepare a loopback template renderer
       const renderer = loopback.template(
-        path.resolve(__dirname, '../../server/views/resetPassword.ejs')
-      );
+          path.resolve(__dirname, '../../server/views/resetPassword.ejs'));
       const html_body = renderer({resetUrl});
       const options = {
         type: 'email',
@@ -79,11 +78,8 @@ class user {
     next();
   }
 
-  public afterRemoteChangePassword(
-    ctx: any,
-    reuslt: any,
-    next: Function
-  ): void {
+  public afterRemoteChangePassword(ctx: any, reuslt: any, next: Function):
+      void {
     const userId = ctx.args.id;
     this.model.findById(userId, {}, (err: any, userInstance: any) => {
       if (err) {
@@ -94,16 +90,16 @@ class user {
         const devAccessTokens = userInstance.devAccessTokens;
         if (devAccessTokens) {
           this.model.app.models.AccessToken.create(
-            devAccessTokens,
-            (error: any, result: any) => {
-              if (err) next(error, result);
-              else next();
-              console.log(
-                'Successfully restored devAccessTokens in AccessToken model.'
-              );
-            }
-          );
-        } else next();
+              devAccessTokens, (error: any, result: any) => {
+                if (err)
+                  next(error, result);
+                else
+                  next();
+                console.log(
+                    'Successfully restored devAccessTokens in AccessToken model.');
+              });
+        } else
+          next();
       }
     });
   }
@@ -112,6 +108,7 @@ class user {
     if (ctx.instance) {
       ctx.instance.createdAt = new Date();
       ctx.instance.email = ctx.instance.email.toLocaleLowerCase();
+      ctx.instance.emailVerified = true;
     }
     console.log('user: Before Save');
     next();
@@ -120,13 +117,11 @@ class user {
   public afterRemoteLogin(ctx: any, loggedUser: any, next: any) {
     // Update the last login date
     this.model.findById(loggedUser.userId, (err: any, user: any) => {
-      if (err) console.error(err);
+      if (err)
+        console.error(err);
       else
         user.updateAttribute(
-          'loggedAt',
-          new Date(),
-          (err: any, user: any) => {}
-        );
+            'loggedAt', new Date(), (err: any, user: any) => {});
     });
     next();
   }
@@ -140,88 +135,85 @@ class user {
 
     // Check if any user exists
     this.model.count((err: any, countUser: any) => {
-      if (err) console.error(err);
+      if (err)
+        console.error(err);
       else {
         if (countUser === 1) {
           // Create admin user
           this.model.app.models.Role.findOne(
-            {where: {name: 'admin'}}, // Find
-            (err: any, instance: any) => {
-              // Callback
-              if (err) console.error('Error creating role', err);
-              else {
-                instance.principals.create(
-                  {
-                    principalType: this.model.app.models.RoleMapping.USER,
-                    principalId: userInstance.id,
-                  },
-                  (err: any, principalInstance: any) => {
-                    if (err) console.log(err);
-                    else {
-                      console.log(principalInstance);
-                      next();
-                    }
-                  }
-                );
-              }
-            }
-          );
+              {where: {name: 'admin'}},  // Find
+              (err: any, instance: any) => {
+                // Callback
+                if (err)
+                  console.error('Error creating role', err);
+                else {
+                  instance.principals.create(
+                      {
+                        principalType: this.model.app.models.RoleMapping.USER,
+                        principalId: userInstance.id,
+                      },
+                      (err: any, principalInstance: any) => {
+                        if (err)
+                          console.log(err);
+                        else {
+                          console.log(principalInstance);
+                          next();
+                        }
+                      });
+                }
+              });
         } else {
           // Create normal user
           this.model.app.models.Role.findOrCreate(
-            {where: {name: 'user'}}, // Find
-            userRole, // Create
-            (err: any, instance: any, created: boolean) => {
-              // Callback
-              if (err) console.error('Error creating role', err);
-              else if (created) {
-                console.log('Created role', instance);
-                instance.principals.create(
-                  {
-                    principalType: this.model.app.models.RoleMapping.USER,
-                    principalId: userInstance.id,
-                  },
-                  (err: any, principalInstance: any) => {
-                    if (err) console.error(err);
-                    else {
-                      console.log(principalInstance);
-                      next();
-                    }
-                  }
-                );
-              } else {
-                instance.principals.create(
-                  {
-                    principalType: this.model.app.models.RoleMapping.USER,
-                    principalId: userInstance.id,
-                  },
-                  (err: any, principalInstance: any) => {
-                    if (err) console.log(err);
-                    else {
-                      console.log(principalInstance);
-                      next();
-                    }
-                  }
-                );
-              }
-            }
-          );
+              {where: {name: 'user'}},  // Find
+              userRole,                 // Create
+              (err: any, instance: any, created: boolean) => {
+                // Callback
+                if (err)
+                  console.error('Error creating role', err);
+                else if (created) {
+                  console.log('Created role', instance);
+                  instance.principals.create(
+                      {
+                        principalType: this.model.app.models.RoleMapping.USER,
+                        principalId: userInstance.id,
+                      },
+                      (err: any, principalInstance: any) => {
+                        if (err)
+                          console.error(err);
+                        else {
+                          console.log(principalInstance);
+                          next();
+                        }
+                      });
+                } else {
+                  instance.principals.create(
+                      {
+                        principalType: this.model.app.models.RoleMapping.USER,
+                        principalId: userInstance.id,
+                      },
+                      (err: any, principalInstance: any) => {
+                        if (err)
+                          console.log(err);
+                        else {
+                          console.log(principalInstance);
+                          next();
+                        }
+                      });
+                }
+              });
         }
       }
     });
 
     // Send mail
-    if (
-      process.env.MAILGUN_API_KEY &&
-      process.env.MAILGUN_DOMAIN &&
-      process.env.MAILGUN_REGION &&
-      process.env.BASE_URL
-    ) {
+    if (process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN &&
+        process.env.MAILGUN_REGION && process.env.BASE_URL) {
       const verificationToken = generateVerificationToken();
       userInstance.updateAttributes({verificationToken});
 
-      // Create a custom object your want to pass to the email template. You can create as many key-value pairs as you want
-      // const verificationUrl =
+      // Create a custom object your want to pass to the email template. You can
+      // create as many key-value pairs as you want const verificationUrl =
       //   process.env.API_URL +
       //   '/api/users/confirm?uid=' +
       //   userInstance.id +
@@ -229,19 +221,15 @@ class user {
       //   verificationToken +
       //   '&redirect=' +
       //   process.env.BASE_URL;
-      const verificationUrl =
-        process.env.API_URL +
-        '/api/users/confirm?uid=' +
-        userInstance.id +
-        '&token=' +
-        verificationToken +
-        '&redirect=https://sigfox.page.link/app';
+      const verificationUrl = process.env.API_URL +
+          '/api/users/confirm?uid=' + userInstance.id +
+          '&token=' + verificationToken +
+          '&redirect=https://sigfox.page.link/app';
       const customMessage = {verificationUrl};
 
       // Prepare a loopback template renderer
       const renderer = loopback.template(
-        path.resolve(__dirname, '../../server/views/verify.ejs')
-      );
+          path.resolve(__dirname, '../../server/views/verify.ejs'));
       const html_body = renderer(customMessage);
 
       const options = {
@@ -275,10 +263,8 @@ class user {
           console.error(err);
         });*/
 
-      /*userInstance.verify(options, (err: any, response: any, next: Function) => {
-        if (err) {
-          console.log(err);
-          ctx.res.status(err.status || 500);
+      /*userInstance.verify(options, (err: any, response: any, next: Function)
+      => { if (err) { console.log(err); ctx.res.status(err.status || 500);
         }
         console.log('> verification email sent:', response);
       });*/
@@ -293,21 +279,19 @@ class user {
     const userId = ctx.args.id;
 
     User.findOne(
-      {where: {id: userId}, include: 'Organizations'},
-      (err: any, user: any) => {
-        if (!err && user && user.Organizations) {
-          user.toJSON().Organizations.forEach((orga: any) => {
-            user.Organizations.remove(orga.id, (err: any, result: any) => {
-              if (!err) {
-                console.log(
-                  'Unlinked user from organization (' + orga.name + ')'
-                );
-              }
+        {where: {id: userId}, include: 'Organizations'},
+        (err: any, user: any) => {
+          if (!err && user && user.Organizations) {
+            user.toJSON().Organizations.forEach((orga: any) => {
+              user.Organizations.remove(orga.id, (err: any, result: any) => {
+                if (!err) {
+                  console.log(
+                      'Unlinked user from organization (' + orga.name + ')');
+                }
+              });
             });
-          });
-        }
-      }
-    );
+          }
+        });
     next();
   }
 
@@ -332,9 +316,7 @@ class user {
     const userId = ctx.args.id;
 
     RoleMapping.destroyAll(
-      {principalId: userId},
-      (error: any, result: any) => {}
-    );
+        {principalId: userId}, (error: any, result: any) => {});
     Category.destroyAll({userId: userId}, (error: any, result: any) => {});
     Device.destroyAll({userId: userId}, (error: any, result: any) => {});
     Message.destroyAll({userId: userId}, (error: any, result: any) => {});
@@ -343,8 +325,9 @@ class user {
     Geoloc.destroyAll({userId: userId}, (error: any, result: any) => {});
     Connector.destroyAll({userId: userId}, (error: any, result: any) => {});
     AccessToken.destroyAll({userId: userId}, (error: any, result: any) => {});
-    // this.model.app.models.Dashboard.destroyAll({userId: userId}, (error: any, result: any) => { });
-    // this.model.app.models.Widget.destroyAll({userId: userId}, (error: any, result: any) => { });
+    // this.model.app.models.Dashboard.destroyAll({userId: userId}, (error: any,
+    // result: any) => { }); this.model.app.models.Widget.destroyAll({userId:
+    // userId}, (error: any, result: any) => { });
 
     next(null, 'Success');
   }
